6eb78602b02cdf8a4169181b2b75d27be411ca9f
TheMageKing <calumlikesapplepie@gmail.com>
2020-01-16 20:56:31 -0500

    Add base of a simple auto routine

--- src/main/java/frc/robot/commands/TurnAroundCommand.java
+++ src/main/java/frc/robot/commands/TurnAroundCommand.java
@@ -10,39 +10,35 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.NavXSubsystem;
 
-public class DriveForwardCommand extends Command {
-  private static final int driveDistance = 14000;
+public class TurnAroundCommand extends Command {
   DriveSubsystem driveSubsystem = Robot.driveSubsystem;
-  int startPoint;
-  public DriveForwardCommand() {
-    requires(Robot.driveSubsystem);
+  NavXSubsystem navX = Robot.navXSubsystem;
+  static final double targetFacing = 180;
+  public TurnAroundCommand() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
-
-    startPoint = 0;
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    //This is to allow me to use the same command for the first and second leg
-    startPoint = driveSubsystem.getLeftEncoder();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    driveSubsystem.ManualDrive(.5, 0);
+    driveSubsystem.ManualDrive(0, .5);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    if(driveSubsystem.getLeftEncoder() >= driveDistance+startPoint){
-      return true;//exit method and command
+    if(navX.getYaw() > targetFacing){
+      return true;
     }
-    return false; //keep going
+    return false;
   }
 
   // Called once after isFinished returns true
