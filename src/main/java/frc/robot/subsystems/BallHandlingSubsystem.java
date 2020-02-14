8d7ab3cc6701776330fa1b082e8297a324253969
TheMageKing <calumlikesapplepie@gmail.com>
2020-02-13 20:09:47 -0500

    Flesh out BallHandling a bit more

--- src/main/java/frc/robot/subsystems/BallHandlingSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallHandlingSubsystem.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -23,13 +24,23 @@ public class BallHandlingSubsystem extends Subsystem {
   WPI_TalonSRX neckMotor = new WPI_TalonSRX(RobotMap.neckMotor);
   //potential solenoid 
 
+  BallHandlingSubsystem(){
+    hopperMotor.setNeutralMode(NeutralMode.Brake);
+    neckMotor.setNeutralMode(NeutralMode.Brake);
+  }
+
   /**
    * We should look into being able to summon a single ball: first we need to see how
    * grippy the ball handling belts are
    */
   public void runMotors(){
     hopperMotor.set(.2);
-    neckMotor.set(.3);
+    neckMotor.set(.3);//magic numbers because this is a skeleton of a class
+  }
+
+  public void stopMotors(){
+    hopperMotor.set(0);
+    neckMotor.set(0);
   }
 
   @Override
