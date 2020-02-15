b22a96029c246020adf0fe5e32da7afb5e8c060e
TheMageKing <calumlikesapplepie@gmail.com>
2020-02-15 14:54:08 -0500

    commit

---
+++ src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ShuffleboardSubsystem extends Subsystem{
+
+    public ShuffleboardSubsystem(){
+
+    }
+
+
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+      }
+
+}
