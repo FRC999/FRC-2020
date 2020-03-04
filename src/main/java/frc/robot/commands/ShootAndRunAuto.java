5666218707463bcdb24d5b58b54576a9cdcd6ef0
rcarten <rgcarten@gmail.com>
2020-03-03 19:48:19 -0500

    Made autonomous easier to code with

--- src/main/java/frc/robot/commands/ShootAndRunAuto.java
+++ src/main/java/frc/robot/commands/ShootAndRunAuto.java
@@ -14,11 +14,11 @@ public class ShootAndRunAuto extends CommandGroup {
 
   //TODO: Add shooting code to autonomous sequence
 
-  public static int startingRotation = 1;
+  public static int startingRotation = 0;
   // 1 is pointing towards the target, 2 is 90 degrees to the right of 1, 3 is 180 degrees, 4 is 270 degrees
 
   public static double xPositionIn = 0;
-  public static double yPositionIn = 228.59;
+  public static double yPositionIn = 0;
   // X position is distance from the starting line (negative X pointing towards the target)
   /* Y position is distance from the far wall of the field -- the side with the other team's ball dispenser
       Field is 323.25 in tall
@@ -26,7 +26,13 @@ public class ShootAndRunAuto extends CommandGroup {
       276.75 + RobotMap.robotLength / 2 when the front is aligned with the outer line of the trench
       161.75 when the center of the robot is aligned with the center of the starting line*/
 
-  public ShootAndRunAuto(boolean shootingFromCenter) {
+  public ShootAndRunAuto(double startingYPosition, int initialRotation, boolean shootingFromCenter) {
+
+    yPositionIn = startingYPosition;
+    startingRotation = initialRotation;
+    if(startingRotation != 1 && startingRotation != 2 && startingRotation != 3 && startingRotation != 4){
+      System.out.println("Auto: Something's wrong with initial rotation; should be 1/2/3/4");
+    }
 
     /* Robot shoots its pre-loaded balls */
     addSequential(new DriveSequentialZeroEncoders());
@@ -63,7 +69,13 @@ public class ShootAndRunAuto extends CommandGroup {
     addSequential(new DriveSequentialTurn(-150));
   }
 
-  public ShootAndRunAuto(boolean shootingFromCenter, double yPositionBuddy) {
+  public ShootAndRunAuto(double startingYPosition, int initialRotation, boolean shootingFromCenter, double yPositionBuddy) {
+
+    yPositionIn = startingYPosition;
+    startingRotation = initialRotation;
+    if(startingRotation != 1 && startingRotation != 2 && startingRotation != 3 && startingRotation != 4){
+      System.out.println("Auto: Something's wrong with initial rotation; should be 1/2/3/4");
+    }
 
     /* Robot shoots its pre-loaded balls */
     addSequential(new DriveSequentialZeroEncoders());
